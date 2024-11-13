997d1c9256721e7c6d962814c28fd05a3e8fea66
Erick <erick.dev.miller@gmail.com>
2024-11-13 11:31:45 +0500

    some updates

--- pages/index.vue
+++ pages/index.vue
@@ -80,12 +80,10 @@
             <div class="chldrensRoom-img">
                 <div class="swiper chldrensRoom-swiper">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
+                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/chldrensRoom_1.jpg" alt=""></div>
+                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/chldrensRoom_2.jpg" alt=""></div>
+                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/chldrensRoom_3.jpg" alt=""></div>
+                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/chldrensRoom_4.jpg" alt=""></div>
                     </div>
                     <div class="swiper-pagination"></div>
                     <div class="swiper-button-prev">
@@ -118,56 +116,7 @@
             </div>
         </div>
     </div>
-    <div id="childrensRoom" class="chldrensRoom">
-        <div class="container chldrensRoom-container chldrensRoom-container-reverse">
-            <div class="chldrensRoom-img">
-                <div class="swiper chldrensRoom-swiper">
-                    <div class="swiper-wrapper">
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                        <div class="swiper-slide chldrensRoom-item"><img src="@/assets/img/remove/menuItem-2.jpeg" alt=""></div>
-                    </div>
-                    <div class="swiper-pagination"></div>
-                    <div class="swiper-button-prev">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-                            <mask id="mask0_472_1837" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0"
-                                width="30" height="30">
-                                <rect width="30" height="30" fill="#D9D9D9" />
-                            </mask>
-                            <g mask="url(#mask0_472_1837)">
-                                <path
-                                    d="M13.0672 14.9999L18.1586 20.0913C18.3316 20.2644 18.4202 20.4819 18.4242 20.7439C18.4282 21.0059 18.3397 21.2275 18.1586 21.4086C17.9775 21.5897 17.7579 21.6802 17.4999 21.6802C17.2419 21.6802 17.0224 21.5897 16.8413 21.4086L11.2235 15.7908C11.1065 15.6738 11.024 15.5504 10.9759 15.4206C10.9278 15.2908 10.9038 15.1506 10.9038 14.9999C10.9038 14.8493 10.9278 14.7091 10.9759 14.5793C11.024 14.4495 11.1065 14.3261 11.2235 14.2091L16.8413 8.59132C17.0144 8.41823 17.2319 8.32969 17.4939 8.32569C17.7559 8.32167 17.9775 8.41021 18.1586 8.59132C18.3397 8.7724 18.4302 8.99194 18.4302 9.24994C18.4302 9.50794 18.3397 9.72748 18.1586 9.90857L13.0672 14.9999Z"
-                                    fill="white" />
-                            </g>
-                        </svg>
-                    </div>
-                    <div class="swiper-button-next">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-                            <mask id="mask0_472_1841" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0"
-                                width="30" height="30">
-                                <rect width="30" height="30" fill="#D9D9D9" />
-                            </mask>
-                            <g mask="url(#mask0_472_1841)">
-                                <path
-                                    d="M16.1828 15L11.0915 9.90861C10.9184 9.73554 10.8299 9.518 10.8259 9.25598C10.8218 8.99398 10.9104 8.77244 11.0915 8.59136C11.2726 8.41025 11.4921 8.3197 11.7501 8.3197C12.0081 8.3197 12.2276 8.41025 12.4087 8.59136L18.0265 14.2091C18.1435 14.3261 18.226 14.4495 18.2741 14.5793C18.3222 14.7091 18.3462 14.8493 18.3462 15C18.3462 15.1506 18.3222 15.2908 18.2741 15.4206C18.226 15.5505 18.1435 15.6738 18.0265 15.7908L12.4087 21.4086C12.2357 21.5817 12.0181 21.6702 11.7561 21.6742C11.4941 21.6783 11.2726 21.5897 11.0915 21.4086C10.9104 21.2275 10.8198 21.008 10.8198 20.75C10.8198 20.492 10.9104 20.2724 11.0915 20.0914L16.1828 15Z"
-                                    fill="white" />
-                            </g>
-                        </svg>
-                    </div>
-                </div>
-            </div>
-            <div class="chldrensRoom-text">
-                <h2>Детская комната в ресторане Public</h2>
-                <p>В нашем ресторане предусмотрена уютная детская комната, где ваши дети смогут весело провести время,
-                    пока вы наслаждаетесь отдыхом. В комнате есть игрушки и развлечения для малышей разных возрастов. За
-                    безопасностью и комфортом детей присматривает опытная няня. Вы можете быть спокойны, зная, что дети
-                    находятся под надежным присмотром в специально оборудованной зоне для игр.</p>
-            </div>
-        </div>
-    </div>
+    
     <div class="formResume">
         <div class="container-fluid formResume-container">
             <div class="formResume-image">
@@ -203,7 +152,7 @@
     </div>
     <div class="shortTextBlock">
         <div class="container shortTextBlock-container">
-            <div class="shortTextBlock-text">
+            <div class="shortTextBlock-text loyal">
                 <h2>Программа лояльности</h2>
                 <p>В нашем ресторане действует накопительная бонусная система! Получайте 5% бонусов на карту с каждого
                     чека при её получении. 1 бонус равен 1 рублю. Бонусами можно оплатить до 100% суммы покупки</p>
@@ -340,6 +289,14 @@ watchEffect(() => {
 
 .galleryItem img {
     // object-fit: contain;   
+}
+
+.chldrensRoom-item {
+    max-height: 500px;
+}
+
+.shortTextBlock-text.loyal {
+    padding-bottom: 30px;
 }
 
 .footerMap {
